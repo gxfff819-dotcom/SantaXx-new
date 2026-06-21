aab25009a51dcb986d525bcaf35e7fd1f18b73d6
gxfff819-dotcom <gxfff819@gmail.com>
2026-06-22 03:24:17 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "BOT_TOKEN",  // Ubah Jadi Token Bot Mu !!!
+  owner: "ID_TELE_LU", // Ubah Jadi Id Mu !!!
+  port: "PORT_PANEL", // Ubah Jadi Port Panel Mu !!!
+  ipvps: "DOMAIN_DI_SINI" // Ubah Jadi Ip Vps Mu !!!
+};
